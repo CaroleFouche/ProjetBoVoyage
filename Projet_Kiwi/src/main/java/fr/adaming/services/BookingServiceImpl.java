d44b8abc367ba7d7d4d4a4789f11c142a4629056
Carole Fouché <IN-BR-007@IN-BR-007>
2019-12-20 12:25:34 +0100

    Carole:

--- Projet_Kiwi/src/main/java/fr/adaming/services/BookingServiceImpl.java
+++ Projet_Kiwi/src/main/java/fr/adaming/services/BookingServiceImpl.java
@@ -50,6 +50,7 @@ public class BookingServiceImpl implements IBookingService {
 	@Override
 	public boolean updateBooking(Booking booking) {
 		
+		
 		Booking bookOut = bookDao.getById(booking.getId());
 		
 		if (bookOut != null) {
