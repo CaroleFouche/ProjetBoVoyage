5476d9eb53b879f8634ccb35cf0dad8ce85a8d7a
Antoine Dubresson <adubresson@ensc.fr>
2019-12-24 16:34:10 +0100

    Antoine : dashboard client connecté + updateCLient (pour que le client puisse modifier ses infos)

--- Projet_Kiwi/src/main/java/fr/adaming/controllers/UserDashboardController.java
+++ Projet_Kiwi/src/main/java/fr/adaming/controllers/UserDashboardController.java
@@ -1,18 +1,98 @@
 package fr.adaming.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import fr.adaming.entities.Client;
+import fr.adaming.services.IClientService;
 
 @Controller
-@RequestMapping(value= {"/user"})
+@RequestMapping(value = { "/user" })
 public class UserDashboardController {
-	
-	
-	@RequestMapping(value= {"dashboard"}, method=RequestMethod.GET)
-	public String getAllDestinations(Model modelDestinations) {
+
+	@Autowired
+	public IClientService clientService;
+
+	public void setClientService(IClientService clientService) {
+		this.clientService = clientService;
+	}
+
+	// Afficher les infos client
+	@RequestMapping(value = { "dashboard" }, method = RequestMethod.GET)
+	public String getClient(Model model) {
+		// TODO : passer le client connecté On récupère le client connecté (ici on écrit
+		// le client en brut)
+		Client cl = new Client("Dubresson", 555, "adubresson@ensc.fr", 24, 14, true, "BGDU33", "a", true);
+		// on ajoute le client passé en brut dans la BD pour pouvoir s'en reservir dans
+		// update
+		clientService.addClient(cl);
+		// On l'envoi dans le model
+		model.addAttribute("logedClient", cl);
 		return "user/dashboard";
 	}
-	
+
+//	@RequestMapping(value = { "dashboard" }, method = RequestMethod.GET)
+//	public String getAllDestinations(Model modelDestinations) {
+//		return "user/dashboard";
+//	}
+
+	/*
+	 * Modifier Client
+	 */
+
+	@RequestMapping(value = { "updateClient" }, method = RequestMethod.GET)
+	public ModelAndView afficherUpdate(@RequestParam(value = "pId", required = false) Integer id,
+			@ModelAttribute("client") Client cl) {
+		
+		if (cl == null) {
+			cl = new Client();
+		}
+		// if client is specified by id, set id
+		if (id != null) {
+			cl.setId(id);
+			cl = clientService.getClientById(cl);
+		}
+		return new ModelAndView("user/updateClient", "client", cl);
+	}
+
+	@RequestMapping(value = { "updateClient" }, method = RequestMethod.POST)
+	public String submitUpdate(RedirectAttributes rda, Model model, @ModelAttribute("client") Client cl) {
+
+		
+		// data validation
+		if (cl == null || cl.getId() == 0) {
+
+			rda.addFlashAttribute("errorMsg", "ID incorrect");
+			rda.addFlashAttribute("errorMsg", cl.getId());
+			rda.addFlashAttribute("client", cl);
+			return "redirect:updateClient";
+		}
+
+		// update
+		boolean result = clientService.updateClient(cl);
+		System.out.println("POST updtae cl");
+
+		// si client modifié redirige sur l'acceuil
+		if (result == true) {
+			rda.addFlashAttribute("successMsg", "Informations modifiées");
+			return "redirect:dashboard";
+
+			// si client pas modifié redirige sur le formulaire d'ajout
+		} else {
+			rda.addFlashAttribute("errorMsg", "La modification n'a pas été effectuée");
+			rda.addFlashAttribute("client", cl);
+			return "redirect:updateClient";
+		}
+
+	}
+
 }
--- Projet_Kiwi/src/main/webapp/WEB-INF/pages/user/dashboard.jsp
+++ Projet_Kiwi/src/main/webapp/WEB-INF/pages/user/dashboard.jsp
@@ -17,9 +17,46 @@
 <body>
 	<!-- Inclure le header -->
 	<%@include file="../../../template/header.html"%>
-	
-	<h2>Afficher les infos pour le client ici (dossier, resas, liens pour les autres pages etc)</h2>
-	
+
+	<h2>Afficher les infos pour le client ici (dossier, resas, liens
+		pour les autres pages etc)</h2>
+	<!-- Afficher la liste des clients-->
+	<div class="container">
+		<table class="table table-bordered">
+			<tr>
+				<td>Actions</td>
+				<th>Numéro Client</th>
+				<th>Nom</th>
+				<th>Tel</th>
+				<th>Mail</th>
+				<th>Age</th>
+				<th>Login</th>
+				<th>Password</th>
+				<th>Carte</th>
+				
+			</tr>
+
+			<tr>
+				<td>
+					<!-- Deux liens pour modifier ou supprimer le client depuis l'accueil de l'admin -->
+					<a href="<c:url value='/user/updateClient?pId=${logedClient.id}'/>">Modifier
+				</a> 
+				</td>
+				<td>${logedClient.id}</td>
+				<td>${logedClient.name}</td>
+				<td>${logedClient.phone}</td>
+				<td>${logedClient.mail}</td>
+				<td>${logedClient.age}</td>
+				<td>${logedClient.login}</td>
+				<td>${logedClient.mdp}</td>
+				<td>${logedClient.numCard}</td>
+				
+			</tr>
+
+
+		</table>
+
+	</div>
 
 	<%@include file="../../../template/footer.html"%>
 </body>
